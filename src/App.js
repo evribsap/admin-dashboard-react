1195143f4743b4feeb410f05206121b8ae7f25ec
evribsap <evribsaputra@gmail.com>
2022-08-19 18:16:42 +0700

    update sidebar & app.js

--- src/App.js
+++ src/App.js
@@ -29,7 +29,7 @@ const App = () => {
           </div>
           {activeMenu ? (
             <div className="w-72 fixed sidebar dark:bg-secondary-dark-bg bg-white">
-              Sidebar
+             <Sidebar />
             </div>
           ) : (
             <div className="w-0 dark:bg-secondary-dark-bg">
